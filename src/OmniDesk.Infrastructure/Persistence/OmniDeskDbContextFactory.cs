51cf530902a3d7c6b8f7cfadc32f53f581ccc246
EricWan9 <b_wang0312@163.com>
2026-09-16 20:34:19 +0800

    feat: add DbContextFactory for EFCore bundle

---
+++ src/OmniDesk.Infrastructure/Persistence/OmniDeskDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OmniDesk.Infrastructure.Persistence;
+
+public sealed class OmniDeskDbContextFactory
+    : IDesignTimeDbContextFactory<OmniDeskDbContext>
+{
+    public OmniDeskDbContext CreateDbContext(string[] args)
+    {
+        const string designTimeConnectionString =
+            "Server=localhost;" +
+            "Database=OmniDesk_DesignTime;" +
+            "Trusted_Connection=True;" +
+            "TrustServerCertificate=True;";
+
+        var options =
+            new DbContextOptionsBuilder<OmniDeskDbContext>()
+                .UseSqlServer(designTimeConnectionString)
+                .Options;
+
+        return new OmniDeskDbContext(options);
+    }
+}
