10cd8d6eefe2dd46d07de199c14f531b50f0c8c1
Eric <77566049+EricWan9@users.noreply.github.com>
2026-09-21 17:12:46 +0800

    fix: Change database connection and clean up code
    Updated database connection string and removed unused JWT configuration.

--- src/OmniDesk.Api/Program.cs
+++ src/OmniDesk.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddControllers();
 builder.Services.AddHealthChecks();
 
 builder.Services.AddDbContext<OmniDeskDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDatabase")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IRegistrationService, RegistrationService>();
 builder.Services.AddScoped<IPasswordService, PasswordService>();
@@ -51,9 +51,6 @@ builder.Services.AddScoped<IMessageAttachmentRepository, MessageAttachmentReposi
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddSignalR();
-
-builder.Services.Configure<JwtOptions>(
-    builder.Configuration.GetSection(JwtOptions.SectionName));
 
 builder.Services.Configure<BlobStorageOptions>(
     builder.Configuration.GetSection(BlobStorageOptions.SectionName));
@@ -168,8 +165,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-// HTTPS is enforced by Azure Container Apps ingress
-// app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseCors();
 
